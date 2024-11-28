f576b24c9960031c6446e96e03d2cd4e29149369
sehunnny <ssh0207798@gmail.com>
2024-11-28 11:45:02 +0900

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,11 +1,8 @@
-import React, { useState, useEffect } from 'react'; // useEffect 추가
-import { useNavigate } from 'react-router-dom';
-
-// 더미 사용자 데이터
-const User = {
-  email: 'UserID@gmail.com',
-  pw: 'usertest123!!',
-};
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
+import logoImage from './images/projectlogo.png';
+import './Login.css';
 
 export default function Login({ onLoginSuccess }) {
   const [email, setEmail] = useState('');
@@ -13,90 +10,108 @@ export default function Login({ onLoginSuccess }) {
   const [emailValid, setEmailValid] = useState(false);
   const [pwValid, setPwValid] = useState(false);
   const [notAllow, setNotAllow] = useState(true);
-
   const navigate = useNavigate();
-
+  const [loading, setLoading] = useState(false);
   const handleEmail = (e) => {
     setEmail(e.target.value);
     const regex =
       /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-    if (regex.test(email)) {
-      setEmailValid(true);
-    } else {
-      setEmailValid(false);
-    }
+    setEmailValid(regex.test(e.target.value));
   };
-
+  
   const handlePassword = (e) => {
     setPw(e.target.value);
     const regex =
       /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
-    if (regex.test(pw)) {
-      setPwValid(true);
-    } else {
-      setPwValid(false);
+    setPwValid(regex.test(e.target.value));
+  };
+
+  const onClickConfirmButton = async () => {
+    const loginInfo = {
+      user_name: email,
+      password: pw
+    };
+
+    setLoading(true);
+    try {
+      const response = await axios.post('http://localhost:8000/auth/login', loginInfo);
+      const accessToken = response.data.access_token;
+      const userId = response.data.user_id;
+      const userName = response.data.user_name;
+
+      if (response.status === 200 && accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('userId', userId);
+        localStorage.setItem('userName', userName);
+        onLoginSuccess(accessToken); // 로그인 성공 시 상태 변경
+        // alert('로그인에 성공했습니다');
+        navigate('/'); // 로그인 성공 후 메인 페이지로 리다이렉트
+      } else {
+        alert('You are not a registered member.');
+        // navigate('/signup'); // 회원가입 페이지로 리다이렉트
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
-  const onClickConfirmButton = () => {
-    if (email === User.email && pw === User.pw) {
-      alert('로그인에 성공했습니다');
-      onLoginSuccess(); // 로그인 성공 시 상태 변경
-      navigate('/'); // 로그인 성공 후 메인 페이지로 리다이렉트
-    } else {
-      alert('등록되지 않은 회원입니다.');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!notAllow) {
+      onClickConfirmButton();
     }
   };
 
-  // 로그인 버튼 비활성화 조건
   useEffect(() => {
-    if (emailValid && pwValid) {
-      setNotAllow(false);
-      return;
-    }
-    setNotAllow(true);
-  }, [emailValid, pwValid]); // 의존성 배열 추가
+    setNotAllow(!(emailValid && pwValid));
+  }, [emailValid, pwValid]);
 
   return (
     <div className="login-page">
-      <div className="titleWrap">이메일과 비밀번호를<br />입력하세요</div>
-
-      <div className="contentWrap">
-        <div className="inputTitle">이메일 주소</div>
-        <div className="inputWrap">
-          <input
-            type="text"
-            className="input"
-            placeholder="UserID@gmail.com"
-            value={email}
-            onChange={handleEmail}
-          />
+      <img src={logoImage} alt='로고' className='logo' />
+      <div className="titleWrap">Enter your email and password</div>
+      <form onSubmit={handleSubmit}>
+        <div className="contentWrap">
+          <div className="inputTitle">Email</div>
+          <div className="inputWrap">
+            <input
+              type="text"
+              className="input"
+              placeholder="UserID@gmail.com"
+              value={email}
+              onChange={handleEmail}
+              style={{ fontSize: '14px' }}
+            />
+          </div>
+          <div className="errorMessageWrap">
+            {!emailValid && email.length > 0 && <div>Please enter a valid email address.</div>}
+          </div>
+          <div style={{ marginTop: '26px' }} className="inputTitle">
+            PassWord
+          </div>
+          <div className="inputWrap">
+            <input
+              type="password"
+              className="input"
+              placeholder="8 more(English, numbers, and special characters)"
+              value={pw}
+              onChange={handlePassword}
+            />
+          </div>
+          <div className="errorMessageWrap">
+            {!pwValid && pw.length > 0 && <div>Please enter the correct password</div>}
+          </div>
         </div>
-        <div className="errorMessageWrap">
-          {!emailValid && email.length > 0 && <div>올바른 이메일을 입력하세요.</div>}
+        <div>
+          <button onClick={onClickConfirmButton} disabled={notAllow} className="bottomButton">
+            Confirm
+          </button>
         </div>
-
-        <div style={{ marginTop: '26px' }} className="inputTitle">
-          비밀번호
-        </div>
-        <div className="inputWrap">
-          <input
-            type="password"
-            className="input"
-            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
-            value={pw}
-            onChange={handlePassword}
-          />
-        </div>
-        <div className="errorMessageWrap">
-          {!pwValid && pw.length > 0 && <div>올바른 비밀번호를 입력하세요</div>}
-        </div>
-      </div>
-
-      <div>
-        <button onClick={onClickConfirmButton} disabled={notAllow} className="bottomButton">
-          확인
-        </button>
+      </form>
+      <div className="link-section" style={{ marginTop: '20px' }}>
+        <Link to="/signup">Signup</Link> {/* 회원가입 페이지로 이동하는 링크 */}
       </div>
     </div>
   );
